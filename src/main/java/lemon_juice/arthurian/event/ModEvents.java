c9510933c9bf9622795eeed6eee5bdef8bf84999
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-05-24 00:41:11 -0400

    Knights Naturally Spawn

--- src/main/java/lemon_juice/arthurian/event/ModEvents.java
+++ src/main/java/lemon_juice/arthurian/event/ModEvents.java
@@ -3,7 +3,11 @@ package lemon_juice.arthurian.event;
 import lemon_juice.arthurian.Arthurian;
 import lemon_juice.arthurian.entity.ModEntities;
 import lemon_juice.arthurian.entity.custom.KnightEntity;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,5 +16,11 @@ public class ModEvents {
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
         event.put(ModEntities.KNIGHT.get(), KnightEntity.setAttributes());
+    }
+
+    @SubscribeEvent
+    public static void entitySpawnRestriction(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.KNIGHT.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Monster::checkMonsterSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 }
